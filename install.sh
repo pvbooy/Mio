d6d9db043d806d4d22253935179ada80f1efd0dc
Boy <129876188+pvbooy@users.noreply.github.com>
2023-11-15 12:38:54 +0330

    Update install.sh
    done

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
-# --@PvBoy--
+@PvBoy/Boy
+
+#!/bin/bash
+
+# پاک کردن صفحه ترمینال
+clear
 
 # اضافه کردن جلوه‌های بصری به پیام‌های سازنده
-clear
 echo -e "\e[1;32m***********************"
-echo -e "*******Made by Boy*******"
+echo -e "***********************"
+echo -e "******Made by Boy******"
+echo -e "***********************"
 echo -e "***********************\e[0m"
 
 # تنظیم ساعت به وقت ایران
@@ -12,42 +18,96 @@ export TZ="Asia/Tehran"
 # نمایش تاریخ و ساعت ایران
 echo -e "\e[1;36m$(date "+%Y/%m/%d|%H:%M:%S")\e[0m"
 
-# نصب مرزبان نود در حال انجام است با جلوه چرخشی
-echo -n "-Node installation is in progress... "
+# بررسی آرگومان خط فرمان برای تعیین نصب یا عدم نصب وارپ
+read -p "آیا مایل به نصب وارپ هستید؟ (y/n): " install_warp
+if [ "$install_warp" == "y" ]; then
+    # نصب وارپ و مراحل بعدی
+    echo -e "\e[1;32mInstalling WireGuard (Warp)..."
+
+    # افزودن مخازن APT
+    sudo add-apt-repository main
+    sudo add-apt-repository universe
+    sudo add-apt-repository restricted
+    sudo add-apt-repository multiverse -y
+
+    # نصب ابزارهای WireGuard و resolvconf
+    sudo apt install wireguard-dkms wireguard-tools resolvconf -y
+
+    # دانلود و نصب wgcf
+    wget https://github.com/ViRb3/wgcf/releases/download/v2.2.18/wgcf_2.2.18_linux_amd64
+    mv wgcf_2.2.18_linux_amd64 /usr/bin/wgcf
+    chmod +x /usr/bin/wgcf
+
+    # ثبت و تولید پروفایل
+    wgcf register --accept-tos
+    wgcf generate
+
+    # انتقال فایل پروفایل به مسیر مورد نظر
+    mv wgcf-profile.conf warp.conf
+    sudo mv warp.conf /etc/wireguard/
+
+    # اعمال تغییرات در فایل تنظیمات WireGuard
+    sudo sed -i '7i\Table = off' /etc/wireguard/warp.conf
+
+    # فعال‌سازی سرویس WireGuard
+    sudo systemctl enable --now wg-quick@warp
+
+    echo -e "WireGuard (Warp) installed successfully.\e[0m"
+else
+    # عدم نصب وارپ و اجرای مراحل بعدی
+    echo -e "\e[1;31mSkipping WireGuard (Warp) installation.\e[0m"
+    # TODO: اضافه کردن دستورات مربوط به مراحل بعدی بدون نصب وارپ
+fi
+
+# جلوه چرخشی و اجرای دستورات مراحل بعدی
 spinner="/|\\-"
 while true; do
     for i in $(seq 0 3); do
         echo -ne "\e[1D${spinner:$i:1}"
         sleep 0.1
     done
+    # TODO: اضافه کردن دستورات مربوط به اضافه کردن فایل‌های مورد نیاز
+    echo -e "\e[1;32mAdding required files..."
+    sudo mkdir -p /usr/local/share/xray/
+    sudo wget -O /usr/local/share/xray/iran.dat https://github.com/bootmortis/iran-hosted-domains/releases/download/202308070029/iran.dat
+    sudo wget -O /usr/local/share/xray/geoip.dat https://github.com/v2fly/geoip/releases/latest/download/geoip.dat
+    sudo wget -O /usr/local/share/xray/geosite.dat https://github.com/v2fly/domain-list-community/releases/latest/download/dlc.dat
+
+    # نصب هسته و ادیتش
+    apt install wget unzip -y
+    mkdir -p /var/lib/marzban/xray-core
+    wget -O /var/lib/marzban/xray-core/Xray-linux-64.zip https://github.com/XTLS/Xray-core/releases/download/v1.8.1/Xray-linux-64.zip
+    unzip /var/lib/marzban/xray-core/Xray-linux-64.zip -d /var/lib/marzban/xray-core
+    rm /var/lib/marzban/xray-core/Xray-linux-64.zip
+
+    # TODO: اضافه کردن دستورات مربوط به نصب مرزبان نود
+    echo -e "\e[1;32mInstalling Marzban Node..."
+    sudo curl -fsSL https://get.docker.com | sh
+    sudo git clone https://github.com/Gozargah/Marzban-node
+    sudo cd Marzban-node
+    sudo docker compose up -d
+    sudo rm Marzban-node/docker-compose.yml
+    sudo wget -O Marzban-node/docker-compose.yml https://phontom.website/docker-compose.yml
+    sudo cd Marzban-node
+    sudo docker compose down
+    sudo docker compose up --remove-orphans -d
+    sudo cat /var/lib/marzban-node/ssl_cert.pem
+
+    # پیام نهایی
+    echo -e "\e[1;32mInstallation completed successfully.\e[0m"
 done
 
-# نصب وارپ
-read -p "آیا مایل به نصب وارپ هستید؟ (y/n): " install_warp
-if [ "$install_warp" == "y" ]; then
-    wget https://github.com/ViRb3/wgcf/releases/download/v2.2.18/wgcf_2.2.18_linux_amd64
-    mv wgcf_2.2.18_linux_amd64 /usr/bin/wgcf
-    chmod +x /usr/bin/wgcf
-    wgcf register --accept-tos
-    wgcf generate
-    sudo apt install wireguard-dkms wireguard-tools resolvconf -y
-    mv wgcf-profile.conf warp.conf
-    sudo mv warp.conf /etc/wireguard/
-    sudo sed -i '7i\Table = off' /etc/wireguard/warp.conf
-    sudo systemctl enable --now wg-quick@warp
-    echo "WireGuard (Warp) installed successfully."
-else
-    echo "Skipping WireGuard (Warp) installation."
-fi
-
-# نصب هسته و ادیتش
-# TODO: اضافه کردن دستورات مربوط به نصب هسته و ادیت هسته
-
-# اضافه کردن فایل‌های مورد نیاز
-# TODO: اضافه کردن دستورات مربوط به اضافه کردن فایل‌های مورد نیاز
-
-# نصب مرزبان نود
-# TODO: اضافه کردن دستورات مربوط به نصب مرزبان نود
 
 
-حالا این اسکریپت ابتدا از کاربر می‌پرسد که آیا می‌خواهید WireGuard (Warp) نصب شود یا خیر. اگر جواب مثبت باشد، این بخش اجرا می‌شود و بعد از آن سایر مراحل اسکریپت به ترتیب اجرا می‌شوند.
+
+
+
+
+
+
+
+
+
+
+
+
