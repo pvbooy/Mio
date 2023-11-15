e5bbdc91b17b5e04e836711f5fa6b655b7abb1f3
Boy <129876188+pvbooy@users.noreply.github.com>
2023-11-15 14:15:14 +0330

    Update install.sh
    fghfghgfhf

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ export TZ="Asia/Tehran"
 echo -e "\e[1;36m$(date "+%Y/%m/%d|%H:%M:%S")\e[0m"
 
 # بررسی آرگومان خط فرمان برای تعیین نصب یا عدم نصب وارپ
-read -p "آیا مایل به نصب وارپ هستید؟ (y/n): " install_warp
+read -p آیا مایل به نصب وارپ هستید؟ (y/n): install_warp
 if [ "$install_warp" == "y" ]; then
     # نصب وارپ و مراحل بعدی
     echo -e "\e[1;32mInstalling WireGuard (Warp)..."
