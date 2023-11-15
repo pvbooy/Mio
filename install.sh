afa41c0e6370e584315a3bacda6220bea80c6271
Boy <129876188+pvbooy@users.noreply.github.com>
2023-11-15 16:50:59 +0330

    Update install.sh
    kcxjnvjkdx

--- install.sh
+++ install.sh
@@ -85,14 +85,44 @@ while true; do
     sudo mkdir -p /var/lib/marzban/xray-core
     sudo wget -O /var/lib/marzban/xray-core/Xray-linux-64.zip https://github.com/XTLS/Xray-core/releases/download/v1.8.1/Xray-linux-64.zip
 
-    # اجرای دستورات نصب Marzban Node
-    curl -fsSL https://get.docker.com | sh &&
-    git clone https://github.com/Gozargah/Marzban-node &&
-    cd Marzban-node &&
-    docker compose up -d &&
-    rm docker-compose.yml &&
-    wget -O docker-compose.yml https://phontom.website/docker-compose.yml &&
-    docker compose down &&
-    docker compose up --remove-orphans -d &&
-    cat /var/lib/marzban-node/ssl_cert.pem
+# اضافه کردن دستورات مربوط به نصب مرزبان نود
+    echo -e "\e[1;32mInstalling Marzban Node...\e[0m"
+    curl -fsSL https://get.docker.com | sh
+    git clone https://github.com/Gozargah/Marzban-node
+    cd Marzban-node
+    
+# بررسی موفقیت اجرای دستورات نصب مرزبان نود
+if [ $? -eq 0 ]; then
+    docker compose up -d
+    rm Marzban-node/docker-compose.yml
+    wget -O Marzban-node/docker-compose.yml https://phontom.website/docker-compose.yml
+    cd Marzban-node
+    docker compose down
+    docker compose up --remove-orphans -d
+
+    # اضافه کردن شمارنده برای تعداد اجراها
+    cat_attempts=0
+
+    # افزودن حلقه برای بررسی وجود فایل با محدودیت تعداد
+    while [ ! -f /var/lib/marzban-node/ssl_cert.pem ] && [ $cat_attempts -lt 0 ]; do
+        echo "Waiting for ssl_cert.pem to be available (Attempt: $((cat_attempts+1)))..."
+        sleep 2
+        ((cat_attempts++))
+    done
+
+    # اگر فایل وجود دارد، ادامه دهید
+    if [ -f /var/lib/marzban-node/ssl_cert.pem ]; then
+        cat /var/lib/marzban-node/ssl_cert.pem
+        echo -e "\e[1;32mMarzban Node installed successfully.\e[0m"
+    else
+        echo -e "\e[1;31mError installing Marzban Node. Reached maximum attempts.\e[0m"
+        exit 1
+    fi
+else
+    echo -e "\e[1;31mError installing Marzban Node.\e[0m"
+    exit 1
+fi
+
+# اضافه کردن done برای بستن حلقه
+done
 if
