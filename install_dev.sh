463abdf9b08da9f3309b6b4a3f9f2fa3f4376874
Boy <129876188+pvbooy@users.noreply.github.com>
2024-04-09 00:50:34 +0330

    Update install_dev.sh

--- install_dev.sh
+++ install_dev.sh
@@ -21,7 +21,7 @@ echo -e "\e[1;36m$(date "+%Y/%m/%d|%H:%M:%S")\e[0m"
 
 # بررسی آرگومان خط فرمان برای تعیین نصب یا عدم نصب وارپ
 read -p "Should Warp be installed? (y/n): " install_warp
-if [ $install_warp == "Yy" ]; then
+if [ $install_warp == "y" ]; then
     # نصب وارپ و مراحل بعدی
     echo -e "\e[1;32mInstalling WireGuard (Warp)..."
 
